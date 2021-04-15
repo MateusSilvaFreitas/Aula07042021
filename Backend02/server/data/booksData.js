3e1337b5e352b047f276467e029338d91dfaee5c
Luiz Kischel <luiz.kischel@hotmail.com>
2021-04-15 19:24:07 -0300

    second comit

--- Backend02/server/data/booksData.js
+++ Backend02/server/data/booksData.js
@@ -13,7 +13,8 @@ exports.deleteBook = function(bookID) {
 }
 
 exports.saveBook = function(book) {
-    return database.one('insert into livro (nomelivros, nomeautor, assunto, editora, isbn, qntdEstoque) values ($1, $2, $3, $4, $5, $6) returning *'
+    return database.one('insert into livros (nomelivros, nomeautor, assunto, editora, isbn, qntdEstoque) values ($1, $2, $3, $4, $5, $6) returning *',
     [book.nomelivros, book.nomeautor, book.assunto, book.editora, book.isbn, book.qntdEstoque]);
 }
 
+de
--- Backend02/server/server.js
+++ Backend02/server/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const booksRoute = require('./route/booksRoute');
 
+app.use(express.json());
+
 app.use(booksRoute);
 app.listen(3333);
 
